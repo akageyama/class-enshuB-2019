478eb8cb9e2d44f7a17728f5b3b78e3034735b7f
Akira Kageyama <sgks@mac.com>
2019-12-29 22:11:20 +0900

    Edit

--- Template/SmoothLife/src/sml.f90
+++ Template/SmoothLife/src/sml.f90
@@ -30,9 +30,6 @@ module sml_m
 
   integer(SI), parameter :: FILE_NUM = 10
 
-  integer(SI), parameter :: PAPERS_RA = 21
-  integer(SI), parameter :: PAPERS_RI = 7
-
 contains
 
   subroutine assert( must_be_true, last_will )
@@ -91,197 +88,6 @@ contains
   end function int_to_str6
 
 
-  function integral( sml, i, j, irad, sheet )
-    type(sml_t), intent(in) :: sml
-    integer(SI), intent(in) :: i, j, irad
-    real(DR), dimension(-irad:irad,-irad:irad), intent(in) :: sheet
-    real(DR) :: integral
-
-    integer(SI) :: ii, jj
-    real(DR) :: sum_f
-
-    sum_f    = 0.0_DR ! reset
-
-    !      *     |     |     |     |     |
-    !      |     *     |     |     |     |
-    !      o-----o---*-o-----o-----o-----o
-    !      |     |     |     |     |     |
-    !      |     |     |  *  |     |     |
-    !      o-----o-----o-----o-----o-----o
-    !      |     |     |     | *   |     |
-    !      |     |     |     |     |     |
-    !      o-----o-----o-----o-----o-----o
-    !      |     |     |     |     *     |
-    !      |     |     |     |     |     |
-    !      o-----o-----o-----o-----o-----o
-    !      |     |     |     |     |  *  |
-    !      |     |     |     |     |     |
-    !      X-----o-----o-----o-----o-----*
-    !       \                             \
-    !        grid=(i,j)                   grid=(i+irad,j)
-    !                     b/2     b/2
-    !                    __|__   __|__
-    !                   /     \ /     \
-    !      +-----------x-------O-------x
-    !      |                   |       |
-    !      |<----------------->|       |
-    !      |       irad                |
-    !      |                           |
-    !      +---------------------------*
-
-    call assert( irad > 0,  &
-                 "<sml/integral> irad <= 0 ?!" )
-    call assert( irad <= PAPERS_RA,  &
-                 "<sml/integral> irad too large." )
-
-    call assert ( i-irad >= 1,          "i-irad out of range" )
-    call assert ( i+irad <= sml%width,  "i+irad out of range" )
-    call assert ( j-irad >= 1,          "j-irad out of range" )
-    call assert ( j+irad <= sml%height, "j+irad out of range" )
-
-    do jj = -irad, +irad
-      do ii = -irad, +irad
-        sum_f = sum_f + sml%f(i+ii,j+jj) * sheet(ii,jj)
-      end do
-    end do
-
-    integral = sum_f
-  end function integral
-
-
-  subroutine make_sheet( irad, sheet )
-    integer(SI), intent(in) :: irad
-    real(DR), dimension(-irad:irad,-irad:irad), intent(out) :: sheet
-    
-    real(DR), parameter :: PAPERS_B = 1.0_DR
-    real(DR) :: papers_ell, rad_plus_b_half, rad_minus_b_half
-    integer(SI) :: i, j
-
-    !      *-----o-----o-----o-----o-----o
-    !      |   * |     |     |     |     |
-    !      |     |     |     |     |     |
-    !      o-----o---*-o-----o-----o-----o
-    !      |     |     |     |     |     |
-    !      |     |     |  *  |     |     |
-    !      o-----o-----o-----o-----o-----o
-    !      |     |     |     | *   |     |
-    !      |     |     |     |     |     |
-    !      o-----o-----o-----o-----o-----o
-    !      |     |     |     |     |*    |
-    !      |     |     |     |     |     |
-    !      o-----o-----o-----o-----o-----o
-    !      |     |     |     |     |   * |
-    !      |     |     |     |     |     |
-    !      X-----o-----o-----o-----o-----*
-    !       \                             \
-    !        center of interest           radius 
-    !                     b/2     b/2
-    !                    __|__   __|__
-    !                   /     \ /     \
-    !      +-----------x-------O-------x
-    !      |                   |       |
-    !      |<----------------->|       |
-    !      |       irad                |
-    !      |                           |
-    !      +---------------------------*
-
-
-    rad_plus_b_half  = real(irad,DR) + PAPERS_B / 2.0_DR
-    rad_minus_b_half = real(irad,DR) - PAPERS_B / 2.0_DR
-
-    do j = -irad, irad
-      do i = -irad, irad
-        papers_ell = iDistance( i, j )
-        if ( papers_ell <= rad_minus_b_half ) then
-          sheet(i,j) = 1.0_DR
-        else if ( rad_minus_b_half < papers_ell .and. &
-                  rad_plus_b_half >= papers_ell ) then
-          sheet(i,j) = (  rad_plus_b_half  &
-                        - papers_ell ) / PAPERS_B
-        else
-          sheet(i,j) = 0.0_DR
-        end if
-      end do
-    end do
-
-  contains
-
-    function iDistance( i, j )
-      integer(SI), intent(in) :: i, j
-      real(DR) :: iDistance
-
-      real(DR) :: x, y
-
-      x = real(i, DR)
-      y = real(j, DR)
-
-      iDistance = sqrt( x*x + y*y )
-    end function iDistance
-    
-  end subroutine make_sheet
-
-
-  function papers_function_sigma1( x, a, alpha )
-    real(DR), intent(in) :: x, a, alpha
-    real(DR) :: papers_function_sigma1
-
-    real(DR) :: denominator
-
-    denominator = 1.0_DR + exp( -4.0_DR*(x-a)/alpha )
-    papers_function_sigma1 = 1.0_DR / denominator
-  end function papers_function_sigma1
-
-
-  function papers_function_sigma2( x, a, b, alpha )
-    real(DR), intent(in) :: x, a, b, alpha
-    real(DR) :: papers_function_sigma2
-
-    real(DR) :: sigma1a, sigma1b
-
-    sigma1a = papers_function_sigma1( x, a, alpha )
-    sigma1b = papers_function_sigma1( x, b, alpha )
-
-    papers_function_sigma2 = sigma1a * ( 1.0_DR - sigma1b )
-  end function papers_function_sigma2
-
-
-  function papers_function_sigma_m( x, y, m )
-    real(DR), intent(in) :: x, y, m
-    real(DR) :: papers_function_sigma_m
-
-    real(DR) :: sigma1
-    real(DR), parameter :: PAPERS_ALPHA_M = 0.147_DR
-
-    sigma1 = papers_function_sigma1( m, 0.5_DR, &
-                                     PAPERS_ALPHA_M )
-
-    papers_function_sigma_m = x * ( 1 - sigma1 ) + y * sigma1
-  end function papers_function_sigma_m
-
-
-  function papers_function_s( n, m )
-    real(DR), intent(in) :: n, m
-    real(DR) :: papers_function_s
-
-    real(DR), parameter :: PAPERS_B1 = 0.278_DR
-    real(DR), parameter :: PAPERS_B2 = 0.365_DR
-    real(DR), parameter :: PAPERS_D1 = 0.267_DR
-    real(DR), parameter :: PAPERS_D2 = 0.445_DR
-    real(DR), parameter :: PAPERS_ALPHA_N = 0.028_DR
-    real(DR) :: sigma_m_1, sigma_m_2
-
-    sigma_m_1 = papers_function_sigma_m( PAPERS_B1,  &
-                                         PAPERS_D1,  &
-                                         m )
-    sigma_m_2 = papers_function_sigma_m( PAPERS_B2,  &
-                                         PAPERS_D2,  &
-                                         m )
-    papers_function_s = papers_function_sigma2( n,  &
-                                                sigma_m_1,  &
-                                                sigma_m_2,  &
-                                                PAPERS_ALPHA_N )
-  end function papers_function_s
-
   function double_sigmoid( x, a, b )
     real(DR), intent(in) :: x, a, b
     real(DR) :: double_sigmoid
@@ -327,22 +133,10 @@ contains
     type(sml_t), intent(inout) :: sml
 
     integer(SI) :: i, j
-    real(DR) :: ra_sq, ri_sq, factor_n, factor_m
-    real(DR) :: s, integral_ri, integral_ra
-    real(DR) :: s1, s2
+    real(DR) :: s, s1, s2
     real(DR) :: papers_m, papers_n
     logical, save :: first_time = .true.
-    real(DR), dimension( -PAPERS_RA:PAPERS_RA,  &
-                         -PAPERS_RA:PAPERS_RA ), save :: sheet_ra
-    real(DR), dimension( -PAPERS_RI:PAPERS_RI,  &
-                         -PAPERS_RI:PAPERS_RI ), save :: sheet_ri
     type(sml_t), save :: sml_copy
-
-    if ( first_time ) then
-      call make_sheet( PAPERS_RI, sheet_ri )
-      call make_sheet( PAPERS_RA, sheet_ra )
-      first_time = .false.
-    end if
 
     sml_copy = sml
 
@@ -358,41 +152,17 @@ contains
     !...-o------o-------o-------o-------o-------o-------o
     !   w-6    w-5     w-4     w-3     w-2     w-1      w
 
-    ri_sq = real(PAPERS_RI, DR)**2
-    ra_sq = real(PAPERS_RA, DR)**2
-
-    factor_m = 1.0_DR / ( PI*ri_sq )
-    factor_n = 1.0_DR / ( PI*(ra_sq-ri_sq) )
-
     !$omp parallel do
     do j = 2, sml%height-1
       do i = 2, sml%width-1
-        !> integral_ri = integral( sml_copy, i, j, PAPERS_RI, sheet_ri )
-        !> integral_ra = integral( sml_copy, i, j, PAPERS_RA, sheet_ra )
-        !> papers_m = integral_ri * factor_m
-        !> papers_n = ( integral_ra - integral_ri ) * factor_n
-        !> s = papers_function_s( papers_n, papers_m )
-        !- s = ( sml_copy%f(i-1,j-1) + sml_copy%f(i  ,j-1)  &
-        !-     + sml_copy%f(i+1,j-1) + sml_copy%f(i+1,j  )  &
-        !-     + sml_copy%f(i+1,j+1) + sml_copy%f(i  ,j+1)  &
-        !-     + sml_copy%f(i-1,j+1) + sml_copy%f(i-1,j  ) ) / ( sml_copy%f(i,j) + 3 )
         s = ( sml_copy%f(i-1,j-1) + sml_copy%f(i  ,j-1)  &
             + sml_copy%f(i+1,j-1) + sml_copy%f(i+1,j  )  &
             + sml_copy%f(i+1,j+1) + sml_copy%f(i  ,j+1)  &
             + sml_copy%f(i-1,j+1) + sml_copy%f(i-1,j  ) ) - ( 8 * sml_copy%f(i,j) )
-!        if ( s >= 0.4_DR .and. s <= 0.9_DR ) then
-!          s = 1.0_DR
-!        else
-!          s = 0.0_DR
-!        end if
-        ! s = double_sigmoid( s, 0.5000_DR, 0.90_DR )
-        ! s = double_sigmoid( s, 0.5000_DR, 0.80_DR )
         s1 = double_sigmoid( s, -6.5_DR , -4.5_DR )
-        s2 = double_sigmoid( s, 2.5_DR , 3.5_DR )
-        !< 1sml%f(i,j) = sml%f(i,j) + 0.02_DR*sml%f(i,j)*(2*s-1)
-        !< 1if ( sml%f(i,j) > 1.0_DR ) then
-        !< 1  sml%f(i,j) = 1.0_DR
-        !< 1end if
+        s2 = double_sigmoid( s,  2.5_DR ,  3.5_DR )
+        s1 = double_sigmoid( s, -6.4_DR , -4.6_DR )
+        s2 = double_sigmoid( s,  2.4_DR ,  3.5_DR )
         sml%f(i,j) = s1 + s2
 !       call assert ( s >= 0.0_DR .and. s <= 1.0_DR,  &
 !                    "<sml__advance> s out of range.")
@@ -420,8 +190,8 @@ contains
 
   subroutine sml__set_by_program( sml )
     type(sml_t), intent(out) :: sml
-    integer(SI) :: width  = 250
-    integer(SI) :: height = 150
+    integer(SI) :: width  = 200
+    integer(SI) :: height = 200
 
     integer(SI) :: i, j, i2, j2, skip
     integer(SI) :: some_non_negative_int
